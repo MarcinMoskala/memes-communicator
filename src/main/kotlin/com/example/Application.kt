70d0523294f1d3fb7be378ee27af299849ffcf77
Marcin Moskala <marcin.moskala@allegro.pl>
2020-11-05 12:19:54 +0100

    Add web sockets

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,11 +1,21 @@
 package com.example
 
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import com.google.gson.Gson
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.http.ContentType
+import io.ktor.http.cio.websocket.DefaultWebSocketSession
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.pingPeriod
+import io.ktor.http.cio.websocket.readText
+import io.ktor.response.respondText
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.websocket.WebSockets
+import io.ktor.websocket.webSocket
+import java.time.Duration
 
 fun main(args: Array<String>) {
     val jsonResponse = """{
@@ -13,15 +23,53 @@ fun main(args: Array<String>) {
         "task": "Pay waterbill",
         "description": "Pay water bill today",
     }"""
-    val port = Integer.valueOf(System.getenv("PORT"))
-    embeddedServer(Netty, port) {
+    embeddedServer(Netty, 8080) {
+        install(WebSockets) {
+            pingPeriod = Duration.ofMinutes(1)
+        }
+
         install(Routing) {
+            val memes = mutableListOf<Meme>()
+            val connections = mutableListOf<DefaultWebSocketSession>()
+
             get("/") {
                 call.respondText("Hello World!", ContentType.Text.Plain)
             }
             get("/todo") {
                 call.respondText(jsonResponse, ContentType.Application.Json)
             }
+            webSocket("/ws") {
+                connections += this
+                outgoing.send(Memes(memes).toJson().let(Frame::Text))
+                try {
+                    for (msg in incoming) {
+                        when (msg) {
+                            is Frame.Text -> {
+                                val meme = msg.readText().fromJson<Meme>()
+                                memes += meme
+                                connections.forEach {
+                                    it.send(Memes(memes).toJson().let(Frame::Text))
+                                }
+                            }
+                        }
+                    }
+                } finally {
+                    connections -= this
+                }
+            }
         }
     }.start(wait = true)
 }
+
+fun Any.toJson(): String = Gson().toJson(this)
+inline fun <reified T> String.fromJson(): T = Gson().fromJson(this, T::class.java)
+
+class Memes(
+    val memes: List<Meme>
+)
+
+class Meme(
+    val author: String,
+    val text: String?,
+    val imgSrc: String?,
+)
